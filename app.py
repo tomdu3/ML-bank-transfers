18cab7da239ad308180e9a2a8fca99eec57187fe
Tomislav Dukez <tomdu3@ymail.com>
2023-10-11 23:09:14 +0100

    add template and logic for displaying fraud/nofraud

--- app.py
+++ app.py
@@ -1,9 +1,22 @@
-#!/usr/bin/env python
-
-from flask import Flask
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
-@app.route("/")
-def hello_world():
-    return "<p>Hello, World!</p>"
+@app.route('/', methods=['GET', 'POST'])
+def transaction():
+
+    if request.method == 'POST':
+        is_fraudulent = False  # Set the default value to False
+        print('ok!')
+        # typeofaction = request.form.get('typeofaction')
+        # etc
+        # if result fraudolent
+        is_fraudulent = True
+
+        return render_template('index.html', is_fraudulent=is_fraudulent)
+
+    # no warning message at the beginning
+    return render_template('index.html', is_fraudulent=None)
+
+if __name__ == '__main__':
+    app.run(debug=True)
