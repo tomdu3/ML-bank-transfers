bd7c527fefbe288a868f31d58b41723f61b7d5ea
Tomislav Dukez <tomdu3@ymail.com>
2023-10-12 23:53:24 +0100

    add new notebook and working deployment

--- app.py
+++ app.py
@@ -1,21 +1,47 @@
 from flask import Flask, render_template, request
+import numpy as np
+import pandas as pd
+from joblib import load
+from datetime import datetime
 
 app = Flask(__name__)
 
 @app.route('/', methods=['GET', 'POST'])
 def transaction():
 
+    model = load('models/model.joblib')
+
     if request.method == 'POST':
         is_fraudulent = False  # Set the default value to False
-        print('ok!')
-        # typeofaction = request.form.get('typeofaction')
-        # etc
-        # if result fraudolent
-        is_fraudulent = True
+        typeofaction = 1 if request.form.get('typeofaction')=='Cash-In' else 2
+        sourceid = int(request.form.get('sourceid'))
+        destinationid = int(request.form.get('destinationid'))
+        amountofmoney = int(request.form.get('amountofmoney'))
+        date_str = request.form.get('date')
+        date_obj = datetime.strptime(date_str, '%Y-%m-%d')
+        date_str = date_obj.strftime('%Y%m%d')
+        date_as_int = int(date_str)
+        print(date_as_int)
+        preds = model.predict([[
+             typeofaction,
+             sourceid,
+             destinationid,
+             amountofmoney,
+             date_as_int
+        ]])
+        print([
+             typeofaction,
+             sourceid,
+             destinationid,
+             amountofmoney,
+             date_as_int
+        ])
+        print(preds)
+        is_fraudulent = 'fraud' == preds[0]
 
         return render_template('index.html', is_fraudulent=is_fraudulent)
 
-    # no warning message at the beginning
+    
     return render_template('index.html', is_fraudulent=None)
 
 if __name__ == '__main__':
---
+++ request.py
@@ -0,0 +1,5 @@
+import requests
+
+# URL
+url = 'http://localhost:5000/api'
+
