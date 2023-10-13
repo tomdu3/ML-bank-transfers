47759b257a312ba37165a042b691f28691d384f7
Tomislav Dukez <tomdu3@ymail.com>
2023-10-13 07:50:10 +0100

    Add validators

--- app.py
+++ app.py
@@ -1,10 +1,12 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, flash
 import numpy as np
 import pandas as pd
 from joblib import load
 from datetime import datetime
 
 app = Flask(__name__)
+app.secret_key = 'my_secret_key'
+
 
 @app.route('/', methods=['GET', 'POST'])
 def transaction():
@@ -13,36 +15,45 @@ def transaction():
 
     if request.method == 'POST':
         is_fraudulent = False  # Set the default value to False
-        typeofaction = 1 if request.form.get('typeofaction')=='Cash-In' else 2
-        sourceid = int(request.form.get('sourceid'))
-        destinationid = int(request.form.get('destinationid'))
-        amountofmoney = int(request.form.get('amountofmoney'))
+        invalid_input = False  
+        typeofaction = request.form.get('typeofaction')
+        sourceid = request.form.get('sourceid')
+        destinationid = request.form.get('destinationid')
+        amountofmoney = request.form.get('amountofmoney')
+
+        if not sourceid.isdigit():
+            flash('Invalid input for Source ID')
+            invalid_input = True
+        if not destinationid.isdigit():
+            flash('Invalid input for Destination ID')
+            invalid_input = True
+        if not amountofmoney.isdigit():
+            flash('Invalid input for Amount of Money')
+            invalid_input = True
+
+
+        if invalid_input:
+            return render_template('index.html', is_fraudulent=None)
+
         date_str = request.form.get('date')
         date_obj = datetime.strptime(date_str, '%Y-%m-%d')
         date_str = date_obj.strftime('%Y%m%d')
         date_as_int = int(date_str)
-        print(date_as_int)
+
         preds = model.predict([[
-             typeofaction,
-             sourceid,
-             destinationid,
-             amountofmoney,
-             date_as_int
+            1 if typeofaction == 'Cash-In' else 2,
+            int(sourceid),
+            int(destinationid),
+            int(amountofmoney),
+            date_as_int
         ]])
-        print([
-             typeofaction,
-             sourceid,
-             destinationid,
-             amountofmoney,
-             date_as_int
-        ])
-        print(preds)
+
         is_fraudulent = 'fraud' == preds[0]
 
         return render_template('index.html', is_fraudulent=is_fraudulent)
 
-    
     return render_template('index.html', is_fraudulent=None)
+
 
 if __name__ == '__main__':
     app.run(debug=True)
